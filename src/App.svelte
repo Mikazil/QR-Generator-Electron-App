a6c9292b4b0a8333d16961b81b6829ec3c4bd400
Mika <94830731+Mikazil@users.noreply.github.com>
2023-08-09 15:56:23 +0700

    Created app

---
+++ src/App.svelte
@@ -0,0 +1,41 @@
+<script>
+    let input = "";
+</script>
+
+<div class="container">
+    <h1 class="nameh1">QR Code Generator</h1>
+
+    <div class="input-group">
+        <label class="label">Enter your text here</label>
+        <input
+            autocomplete="off"
+            name="qrtext"
+            id="qrtext"
+            class="input"
+            type="text"
+            bind:value={input}
+        />
+        <div />
+    </div>
+
+    {#if input}
+        <img
+            src="http://qrcoder.ru/code/?{input}&4&0"
+            width="156"
+            height="156"
+            class="qrcode"
+            alt="QR"
+        />
+    {:else}
+        <img
+            src="http://qrcoder.ru/code/?Welcome to the QR code generator&4&0"
+            width="156"
+            height="156"
+            class="qrcode"
+            alt="QR"
+        />
+    {/if}
+</div>
+
+<style>
+</style>
